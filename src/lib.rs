f668cda5007f0c7779ca1d586db3e69d82bb849d
Dowon <ks2515@naver.com>
2026-04-15 13:11:56 +0900

    feat: update boa to 0.21.1 (#13)

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,18 @@
-use pyo3::panic::PanicException;
 use pyo3::prelude::*;
 mod hebi;
 
 #[pymodule]
-fn boabem(py: Python<'_>, m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add("__version__", env!("CARGO_PKG_VERSION"))?;
-    m.add_class::<hebi::PyUndefined>()?;
-    m.add_class::<hebi::PyContext>()?;
-    m.add("PanicException", py.get_type::<PanicException>())?;
-    Ok(())
+mod boabem {
+    #[pymodule_export]
+    #[allow(non_upper_case_globals)]
+    const __version__: &str = env!("CARGO_PKG_VERSION");
+
+    #[pymodule_export]
+    use super::hebi::PyUndefined;
+
+    #[pymodule_export]
+    use super::hebi::PyContext;
+
+    #[pymodule_export]
+    use pyo3::panic::PanicException;
 }
